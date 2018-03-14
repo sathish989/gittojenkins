a44cf8614711ca48a16a104dd8a51380477333c5
satish <sathish9a@gmail.com>
2018-03-14 19:36:50 +0530

    Adding filed from demomars

---
+++ src/main/java/demomars/App.java
@@ -0,0 +1,8 @@
+package demomars;
+
+public class App {
+	public String Sample(){
+		return "Welcome Jenkins";
+	}
+
+}
---
+++ src/test/java/demomars/Apptest.java
@@ -0,0 +1,14 @@
+package demomars;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
